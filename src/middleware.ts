1f0c07c558da54e054de3c0ccd1f00c80d626310
Fatima Iqbal <fatimaiqbalmirza002@gmail.com>
2025-01-23 23:24:16 +0500

    Modify middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,40 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
- 
-export function middleware(request: NextRequest) {
-  const path = request.nextUrl.pathname;
-  const isPublicPath = path === '/login' || path == '/signup';
-  const token = request.cookies.get('token')?.value || '';
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
-  if(isPublicPath && token){
-    return NextResponse.redirect(new URL('/', request.nextUrl))
+export function middleware(req: NextRequest) {
+  const path = req.nextUrl.pathname;
+  const isPublicPath = path === '/login' || path === '/signup';
+  const token = req.cookies.get('token')?.value || '';
+
+  const res = NextResponse.next();
+  res.headers.set('Access-Control-Allow-Credentials', 'true');
+  res.headers.set('Access-Control-Allow-Origin', `${process.env.VERCEL_ENV}`);
+  res.headers.set('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT,OPTIONS'); 
+  res.headers.set(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+
+  if (req.method === 'OPTIONS') { 
+    return res;
+  }
+  
+  if (isPublicPath && token) {
+    return NextResponse.redirect(new URL('/', req.nextUrl));
   }
 
-  if(!isPublicPath && !token){
-    return NextResponse.redirect(new URL('/', request.nextUrl))
+  if (!isPublicPath && !token) {
+    return NextResponse.redirect(new URL('/login', req.nextUrl));
   }
 
+  return res; 
 }
- 
+
 export const config = {
   matcher: [
     '/login',
     '/signup',
-    '/changePassword'
-  ]
-}
+    '/changePassword',
+    '/api/:path*',
+  ],
+};
